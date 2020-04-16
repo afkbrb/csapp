c2176f8edc550b14b02c67c80dc35fbd8e89b726
Lu Liu <2w6f8c@gmail.com>
2020-04-16 12:24:05 +0800

    finish cache lab :)

--- lab06-cache/cachelab-handout/csim.c
+++ lab06-cache/cachelab-handout/csim.c
@@ -1,7 +1,167 @@
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "cachelab.h"
 
-int main()
-{
-    printSummary(0, 0, 0);
+typedef struct Block {
+    unsigned long timestamp;
+    unsigned long tag;
+    bool valid;
+    // we don't need to store data for this lab
+} Block, *BlockPtr;
+
+int s = -1;
+int E = -1;
+int b = -1;
+bool verbose = false;
+
+unsigned counter = 0;
+unsigned hits = 0;
+unsigned misses = 0;
+unsigned evictions = 0;
+
+FILE* tracefile = NULL;
+
+BlockPtr blocks;
+
+void help();
+void parseArgs(int argc, char* argv[]);
+void buildCache();
+void replay();
+
+int main(int argc, char* argv[]) {
+    parseArgs(argc, argv);
+    buildCache();
+    replay();
+    printSummary(hits, misses, evictions);
     return 0;
+}
+
+void usage() {
+    printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+    printf("Options:\n");
+    printf("  -h         Print this help message.\n");
+    printf("  -v         Optional verbose flag.\n");
+    printf("  -s <num>   Number of set index bits.\n");
+    printf("  -E <num>   Number of lines per set.\n");
+    printf("  -b <num>   Number of block offset bits.\n");
+    printf("  -t <file>  Trace file.\n");
+    printf("\n");
+    printf("Examples:\n");
+    printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+    printf("  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+    exit(0);
+}
+
+void parseArgs(int argc, char* argv[]) {
+    int opt = 0;
+    while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+        switch (opt) {
+            case 'h':
+                usage();
+                break;
+            case 'v':
+                verbose = true;
+                break;
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 'E':
+                E = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 't':
+                if (!(tracefile = fopen(optarg, "r"))) {
+                    printf("cannot open file %s\n", optarg);
+                    exit(0);
+                }
+                break;
+            default:
+                usage();
+        }
+    }
+
+    if (s == -1 || E == -1 || b == -1 || !tracefile) {
+        printf("./csim: Missing required command line argument\n");
+        usage();
+    }
+
+    printf("s: %d, E: %d, b: %d, verbose: %d\n", s, E, b, verbose);
+}
+
+void buildCache() {
+    int blockCount = (1 << s) * E;
+    blocks = (BlockPtr)malloc(blockCount * sizeof(Block));
+    for (int i = 0; i < blockCount; i++) {
+        blocks[i].tag = 0;
+        blocks[i].timestamp = 0;
+        blocks[i].valid = false;
+    }
+}
+
+void execute(char operation, unsigned long address, unsigned size) {
+    if (verbose) printf("%c %lx,%d", operation, address, size);
+    unsigned long tag = address >> (s + b);
+    unsigned long setBase = (address << (64 - s - b) >> (64 - s)) * E;
+    // printf("tag: %lx, set: %lx\n", tag, set);
+    int times = operation == 'M' ? 2 : 1;
+    for (int time = 0; time < times; time++) {
+        bool hit = false;
+        for (int i = setBase; i < setBase + E; i++) {
+            if (blocks[i].valid && blocks[i].tag == tag) {
+                hit = true;
+                blocks[i].timestamp = counter++;
+                hits++;
+                if (verbose) printf(" hit");
+                break;
+            }
+        }
+        if (!hit) {
+            misses++;
+            if (verbose) printf(" miss");
+            // load from memory, evict if needed
+            bool full = true;
+            for (int i = setBase; i < setBase + E; i++) {
+                if (!blocks[i].valid) {  // this block is not used yet
+                    full = false;
+                    blocks[i].valid = true;
+                    blocks[i].tag = tag;
+                    blocks[i].timestamp = counter++;
+                    break;
+                }
+            }
+            if (full) {  // evict by LRU
+                evictions++;
+                if (verbose) printf(" eviction");
+                int min = setBase;
+                for (int i = setBase + 1; i < setBase + E; i++) {
+                    if (blocks[i].timestamp < blocks[min].timestamp) {
+                        min = i;
+                    }
+                }
+                blocks[min].tag = tag;
+                blocks[min].timestamp = counter++;
+            }
+        }
+    }
+    if (verbose) printf("\n");
+}
+
+void replay() {
+    char operation;
+    unsigned long address;
+    int size;
+    while (!feof(tracefile)) {
+        if (fscanf(tracefile, " %c %lx,%d", &operation, &address, &size) == 3) {
+            if (operation == 'I') continue;
+            // printf("operation:%c, address:%lx\n", operation, address);
+            execute(operation, address, size);
+        }
+    }
+    fclose(tracefile);
 }
--- lab06-cache/cachelab-handout/trans.c
+++ lab06-cache/cachelab-handout/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,35 +6,57 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
+
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+    int w = 8, h = M == 64 ? 4 : 8;
+    int i, j;
+    for (i = 0; w * i <= M - w; i++) {
+        for (j = 0; h * j <= N - h; j++) {
+            for (int k = 0; k < w; k++) {
+                for (int l = 0; l < h; l++) {
+                    B[w * i + k][h * j + l] = A[h * j + l][w * i + k];
+                }
+            }
+        }
+    }
+
+    for (int x = w * i; x < M; x++) {
+        for (int y = 0; y < N; y++) {
+            B[x][y] = A[y][x];
+        }
+    }
+
+    for (int x = h * j; x < N; x++) {
+        for (int y = 0; y < M; y++) {
+            B[y][x] = A[x][y];
+        }
+    }
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
 
-/* 
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
+void trans(int M, int N, int A[N][M], int B[M][N]) {
     int i, j, tmp;
 
     for (i = 0; i < N; i++) {
@@ -42,8 +64,7 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
+    }
 }
 
 /*
@@ -53,23 +74,20 @@ void trans(int M, int N, int A[N][M], int B[M][N])
  *     performance. This is a handy way to experiment with different
  *     transpose strategies.
  */
-void registerFunctions()
-{
+void registerFunctions() {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+    registerTransFunction(trans, trans_desc);
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
  */
-int is_transpose(int M, int N, int A[N][M], int B[M][N])
-{
+int is_transpose(int M, int N, int A[N][M], int B[M][N]) {
     int i, j;
 
     for (i = 0; i < N; i++) {
@@ -81,4 +99,3 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
     }
     return 1;
 }
-
